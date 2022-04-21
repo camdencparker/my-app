897659a483647b4697cfb0420ef49022f99a0f1a
Camden <camdencparker@gmail.com>
2022-04-21 10:17:10 -0700

    adds square and board components

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 // eslint-disable-next-line
 import "./index.css";
+
+class Square extends React.Component {
+  render() {
+    return <button className="square">{this.props.value}</button>;
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+}
